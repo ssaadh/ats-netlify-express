c4b27e3b3946f11f7931afc669c90654f243557d
Saadh S <atextbooksituation@gmail.com>
2019-03-30 11:55:32 -0400

    Locally if needed

--- server-local.js
+++ server-local.js
@@ -2,4 +2,7 @@
 
 const app = require('./express/server');
 
-app.listen(3000, () => console.log('Local app listening on port 3000!'));
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+const port = ( process.env.NODE_ENV == 'development' ) ? 8000 : 3000;
+
+app.listen(port, () => console.log(`Local app listening on port ${ port }!`));
